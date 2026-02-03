579401c3e47c520edc1ba3bfca8b2ca0ed0e7b75
Tran Nguyen <tran@empowersvc.com>
2026-02-03 17:33:47 -0600

    Add more knowledge

--- ListComprehensive.py
+++ ListComprehensive.py
@@ -35,3 +35,51 @@ print(get_price_tax_service(100))
 
 final_prices = [ get_price_tax_service(bill) for bill in transactions]
 print(final_prices)
+
+# Advanced Functions
+
+#list() --> convert strings ==> List
+my_strings = "Welcome to DataXplore Channel"
+list_of_chars = list(my_strings)
+print(list_of_chars)
+print(list(my_strings))
+
+#sum
+print(sum([1,2,3,4,5]))
+
+#zip(): looping through 2 list simultaneously
+wizards = ['Harry Potter', 'Ron', 'Hermione']
+pets = ['Hedwig', 'Scabber', 'Crookshank']
+
+for wiz, pet in zip(wizards, pets):
+    print(f'{wiz} has {pet}')
+
+
+for index, (wiz, pet) in enumerate(zip(wizards, pets), start = 1):
+    print(f"{index}. {wiz} has {pet}")
+
+#sorted
+sorted_by_first = sorted(['hi', 'hello','you', 'DataXplore'], key = lambda el: el[1])
+print(sorted_by_first)
+
+
+sorted_by_key = sorted([{'name': 'DatXplore', 'age': 15},{'name': 'Andy', 'age': 18},{'name': 'Zoey', 'age': 25}], key = lambda el: el['name'])
+print(sorted_by_key)
+
+# 2D Array/List = Matrix: Mang 2 chieu
+
+matrix = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+print(matrix[1][2])
+
+for row in range(len(matrix)):
+    for col in range(len(matrix)):
+        print(matrix[row][col])
+
+#transform matrix in list
+list_converted = [matrix[row][col] for row in range(len(matrix)) for col in range(len(matrix))]
+print(list_converted)
+
+#combine columns with zip and *:
+print([x for x in zip(*matrix)])
